bc1a2f80efeb5fe343b321a3e7801a1a522c9794
KM <mannka@umich.edu>
2016-05-03 14:58:08 -0400

    Added 1957 data

--- read_gm_data.R
+++ read_gm_data.R
@@ -1,5 +1,7 @@
 # Read data from .csv file
 gdp <- read.csv("data/data-1952.csv")
+new <- read.csv("data/data-1957.csv")
+gdp <- rbind(gdp, new)
 
 # Calculate GDP
 gdp['gdp'] <- gdp$pop * gdp$gdpPercap 
